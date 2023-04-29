69ed3eac68b67eb6dd025524ff86466b0de2e9dd
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2023-04-29 16:45:16 +0900

    Enable listening

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:gpt_eikaiwa/services/speech_to_text.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -10,6 +12,11 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
+  final SpeechToText _speech = SpeechToText();
+
+  void _handleSpeechResult(String result) {
+    _textController.text = result;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,9 +59,18 @@ class _ChatScreenState extends State<ChatScreen> {
                 onSubmitted: _handleSubmitted,
               ),
             ),
-            IconButton(
-              icon: const Icon(Icons.send),
+            PlatformIconButton(
+                onPressed: _toggleListening,
+                material: (_, __) =>
+                    MaterialIconButtonData(icon: const Icon(Icons.mic)),
+                cupertino: (_, __) => CupertinoIconButtonData(
+                    icon: const Icon(CupertinoIcons.mic))),
+            PlatformIconButton(
               onPressed: () => _handleSubmitted(_textController.text),
+              material: (_, __) =>
+                  MaterialIconButtonData(icon: const Icon(Icons.send)),
+              cupertino: (_, __) => CupertinoIconButtonData(
+                  icon: const Icon(CupertinoIcons.arrow_up)),
             ),
           ],
         ),
@@ -65,6 +81,15 @@ class _ChatScreenState extends State<ChatScreen> {
   void _handleSubmitted(String text) {
     _textController.clear();
     // Implement the logic to send the message and update the chat UI
+  }
+
+  void _toggleListening() {
+    if (_speech.isListening()) {
+      _speech.stopListening();
+    } else {
+      _speech.onResult = _handleSpeechResult;
+      _speech.startListening();
+    }
   }
 
   @override
--- lib/services/speech_to_text.dart
+++ lib/services/speech_to_text.dart
@@ -1,23 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class SpeechToText {
   final stt.SpeechToText _speech = stt.SpeechToText();
-  bool _isListening = false;
   String transcription = '';
 
-  void startListening() {
-    if (!_isListening) {
-      _speech.listen(onResult: (result) {
-        transcription = result.recognizedWords;
-      });
-      _isListening = true;
+  void Function(String)? onResult;
+
+  bool isListening() {
+    return _speech.isListening;
+  }
+
+  void startListening() async {
+    bool available = await _speech.initialize();
+    if (available) {
+      _speech.listen(
+        onResult: (result) {
+          transcription = result.recognizedWords;
+          if (onResult != null) {
+            onResult!(transcription);
+          }
+          debugPrint('認識結果： $transcription'); // debugPrintで表示する
+        },
+        listenFor: const Duration(seconds: 15),
+        pauseFor: const Duration(seconds: 3),
+        onSoundLevelChange: null,
+        cancelOnError: true,
+        listenMode: stt.ListenMode.confirmation,
+        // localeId: 'ja-JP'
+      );
+    } else {
+      debugPrint('The user has denied the use of speech recognition.');
     }
   }
 
   void stopListening() {
-    if (_isListening) {
+    if (isListening()) {
       _speech.stop();
-      _isListening = false;
     }
   }
 }
